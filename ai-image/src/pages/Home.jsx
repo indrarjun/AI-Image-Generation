06e0f928fd3768cac91d5c5d35405af4a7fc40b0
Indrarjun <billasaketh@gmail.com>
2023-03-06 20:28:22 +0530

    Added Component files and added a section in Home.jsx

--- ai-image/src/pages/Home.jsx
+++ ai-image/src/pages/Home.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+
+import {Loader, Card, FormField} from '../components';
 
 const Home = () => {
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
+
   return (
-    <div>Home</div>
+    <section className='max-w-7xl mx-auto'>
+      <div>
+        <h1 className='font-extrabold text-[#22328] text-[32px]'>The Community Showcase</h1>        
+      {/* Time Stamp = 25:58 */}
+      </div>
+    </section>
   )
 }
 
